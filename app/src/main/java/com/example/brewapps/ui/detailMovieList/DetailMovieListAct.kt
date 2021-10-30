3f82c9928110a5b82748a6a338cfdd5d1fbb1f7d
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-30 22:59:02 +0530

    recyclerview <wip>

---
+++ app/src/main/java/com/example/brewapps/ui/detailMovieList/DetailMovieListAct.kt
@@ -0,0 +1,12 @@
+package com.example.brewapps.ui.detailMovieList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.brewapps.R
+
+class DetailMovieListAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie_list)
+    }
+}
